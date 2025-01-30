de5e22cfb1f44738f26b22a79fb7adb957698cc1
Christoph Steltner <mail@nplhse.com>
2025-01-30 17:39:54 +0100

    feat: Added basic DefaultController

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/default');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('body', 'Hello DefaultController!');
+    }
+}
