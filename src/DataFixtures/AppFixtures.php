b88226e8d5c8d388fa7405879d212cbabf51f6ca
Christoph Steltner <mail@nplhse.com>
2025-02-15 23:19:46 +0100

    feat: Added doctrine fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->loadUsers($manager);
+    }
+
+    private function loadUsers(ObjectManager $manager): void
+    {
+        foreach ($this->getUserData() as [$username, $password, $email, $fullName, $roles]) {
+            $user = new User();
+            $user->setUsername($username);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+            $user->setEmail($email);
+            $user->setFullName($fullName);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+
+            $this->addReference($username, $user);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return array<array{string, string, string, string, list<string>}>
+     */
+    private function getUserData(): array
+    {
+        return [
+            ['admin', 'password', 'admin@demo.local', 'Administrator', ['ROLE_ADMIN']],
+            ['foo', 'bar', 'foo@demo.local', 'Foo Bar', ['ROLE_USER']],
+        ];
+    }
+}
